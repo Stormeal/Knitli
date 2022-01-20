d7bf506d45d1fcf8c851e337b0c657f52c09e485
Alex Storm <als@edora.dk>
2022-01-20 23:22:27 +0100

    refactor(client): Added responsive mobile-first approach to site

--- client/src/app/home/navigation/navigation.component.ts
+++ client/src/app/home/navigation/navigation.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
+  faBars = faBars;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

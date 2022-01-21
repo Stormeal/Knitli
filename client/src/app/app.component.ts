3967e2cbdd2451f1d3a0861fb3c60778b596111c
Alex Storm <als@edora.dk>
2022-01-21 23:50:39 +0100

    feat(client): Added AOS for animations

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadingService } from './services/loading.service';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loading$ = this.loader.loading$;
   title = 'Knitli';
 
   constructor(public loader: LoadingService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 
   fetchUser() {
     this.http.get('https://api.github.com/users/stormeal').subscribe((res) => {

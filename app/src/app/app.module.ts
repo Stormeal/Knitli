3c619d9ac791aea634c18b82fabf3b2c04046b3c
Alex Storm <as@icrobotics.com>
2022-04-23 23:18:34 +0200

    feat(app): Sidebar and header added

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { ButtonsComponent } from './components/buttons/buttons.component';
 import { CardComponent } from './components/card/card.component';
 import { SidebarComponent } from './layouts/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { HeaderComponent } from './layouts/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonsComponent,
     CardComponent,
-    SidebarComponent
+    SidebarComponent,
+    DropdownComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
